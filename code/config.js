ba462a3619879621ad349de0bee224f5961d537f
twalt1 <31697661+twalt1@users.noreply.github.com>
2018-01-07 14:55:40 -0500

    Create config.js

---
+++ code/config.js
@@ -0,0 +1,8 @@
+//The config file that contains all of the specific user keys related to the Lil Pump Bot
+module.exports = {
+  consumer_key:         'osMPxoN94tsW86rDxnnrxcCel',
+  consumer_secret:      'vycZedFJwOwZkrv4AkDHwTfunvociDqkoXgeyLpDRAw1eJpBHC',
+  access_token:         '943150373342031872-QAiWkx5MqEmNSdCA0zLjm4kDWxGlt5y',
+  access_token_secret:  '5jApjgGkm8YrZsjMb9NpR0zVBT0OURHOOYssZ20SI6m96',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
