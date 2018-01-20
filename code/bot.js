e2521cf7de54ae1d1fd5b60cad6689fef7b66f2a
Anees Aissaoui <31697581+aaiss1@users.noreply.github.com>
2018-01-19 19:57:43 -0500

    Update bot.js

--- code/bot.js
+++ code/bot.js
@@ -1,22 +1,22 @@
 //Our bot that shows you the most recent tweet by @lilpump in the console
 
-console.log('The bot is starting');
+console.log('The bot is starting'); // Notifies you in the console that the bot is starting
 
-var Twit = require('twit');
+var Twit = require('twit'); // Makes sure that the Twit package exists, otherwise, it wont work
 
-var config = require('./config');
+var config = require('./config'); // Makes sure that the config file is there, then it gets the settings from that file
 var T = new Twit(config);
 
-var params = {
-  from: '@lilpump',
+var params = { // A variable that holds all of the parameters
+  from: '@lilpump', // Collects 1 tweet from @LilPump
   count: 1
 }
 
-T.get('search/tweets', params, gotData);
+T.get('search/tweets', params, gotData); // Gets a tweet with the parameters defined in the *params* variable
 
 function gotData(err, data, response) {
-  var tweets = data.statuses;
-  for (var i = 0; i < tweets.length; i++) {
+  var tweets = data.statuses; // Defines statuses as tweets
+  for (var i = 0; i < tweets.length; i++) { // Outputs 1 tweet in the console
     console.log(tweets[i].text);
   }
 }
